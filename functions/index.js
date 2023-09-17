1eac12fc4a6224602af907b6608f48503c9f596c
Ryan McCord <mccordryan00@gmail.com>
2023-09-17 01:22:19 -0500

    fixed index.js

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,21 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const { initializeApp } = require("firebase-admin/app");
+const { onCall } = require("firebase-functions/v2/https");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+initializeApp();
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+exports.createDefaultDeck = functions
+  .runWith({
+    timeoutSeconds: 30,
+    memory: "128MB",
+  })
+  .auth.user()
+  .onCreate(require("./createDefaultDeck"));
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.createCardFromExtension = onCall(
+  {
+    timeoutSeconds: 30,
+    memory: "128MB",
+  },
+  require("./createCardFromExtension")
+);
