ee81c15327b69ff3cdcb6babb63561e0452ebaeb
Paul Chinnam <63817253+paulchinnam@users.noreply.github.com>
2023-09-17 05:21:25 -0500

    removed signup route

--- src/app/page.js
+++ src/app/page.js
@@ -13,11 +13,9 @@ export default function Home() {
     <>
       <div className="flex h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
+          <h1 className="text-indigo-600 font-medium text-2xl text-center">
+            GPTeach
+          </h1>
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign up for an account
           </h2>
--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -12,11 +12,9 @@ export default function Example() {
     <>
       <div className="flex h-screen flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
-          />
+          <h1 className="text-indigo-600 font-medium text-2xl text-center">
+            GPTeach
+          </h1>
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             Sign in to your account
           </h2>
@@ -143,7 +141,7 @@ export default function Example() {
           <p className="mt-10 text-center text-sm text-gray-500">
             Don't have an account?{" "}
             <Link
-              href="/signup"
+              href="/"
               className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
             >
               Sign Up
